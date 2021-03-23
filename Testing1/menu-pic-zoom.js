dfdbfc169b81a55f86ed0864f02806d94779da0b
annebelchoong <annebelchoong@gmail.com>
2021-03-23 16:22:48 +0800

    testing on menu layout

---
+++ Testing1/menu-pic-zoom.js
@@ -0,0 +1,13 @@
+mediumZoom('.zoom', {
+    background: '#000D',
+    opacity: 0.5,
+    margin: 100
+})
+
+
+// mediumZoom('.zoom-item', {
+//     background: '#000',
+//     template:'#zoom-template',
+//     container: '#zoom-container',
+//     background: '#a1a1a1'
+// })
